7ac6bfbc597acda0fa0b2cc97a69fe04951c60b7
hocci <hocci32@gmail.com>
2021-07-14 00:11:47 +0800

    mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,40 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose'
 import dotenv from "dotenv";
+import userRouter from './routers/userRouter.js';
 dotenv.config();
 
 const app = express()
+mongoose.connect('mongodb+srv://peak:the123@cluster0.fkaoh.mongodb.net/amazona?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
 
 app.get('/api/products', (req, res) => {
     res.send(data.products)
 })
 
 app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find( (x) => x._id === req.params.id )
-    if (product){
+    const product = data.products.find((x) => x._id === req.params.id)
+    if (product) {
         res.send(product)
     } else {
-        res.status(404).send({message: 'Product not Found'})
-    }    
+        res.status(404).send({ message: 'Product not Found' })
+    }
 })
+
+app.use('/api/users', userRouter)
 
 app.get('/', (req, res) => {
     res.send("Server is ready")
-} );
+});
+
+app.use((err, req, res, next)=>{
+    res.status(500).send({message: err.message});
+})
 
 const port = process.env.PORT || 5000
 
